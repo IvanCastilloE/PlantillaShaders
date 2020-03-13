bec4c51d34ed634dc95683a82bf42393a1f162dd
alumno <alumno@E303>
2020-03-13 10:49:20 -0700

    Se empieza a incluir las transformaciones

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -16,9 +16,15 @@
 #include "Shader.h"
 #include "Vertice.h"
 #include <vector>
+
+#include "glm/glm.hpp"
+#include "glm/gtx/transform.hpp"
+
 using namespace std;
+using namespace glm;
 //Cada elemento que queramos renderear necesita un vertex array y un buffer
 vector<Vertice> triangulo;
+mat4 transformacionesTriangulo;
 GLuint vertexArrayTrianguloID;
 GLuint bufferTrianguloID;
 
@@ -30,22 +36,23 @@ Shader *shader;
 //Identificadores para mapeo de atributos de entrada del vertex shader
 GLuint posicionID;
 GLuint colorID;
+GLuint transformacionesID;
 
 void inicializarCuadrado() {
 	Vertice v1 = {
-		vec3(-0.2f,0.2f,0.0f),
+		vec4(-0.2f,0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	Vertice v2 = {
-		vec3(0.2f,0.2f,0.0f),
+		vec4(0.2f,0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	Vertice v3 = {
-		vec3(0.2f,-0.2f,0.0f),
+		vec4(0.2f,-0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	Vertice v4 = {
-		vec3(-0.2f,-0.2f,0.0f),
+		vec4(-0.2f,-0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	cuadrado.push_back(v1);
@@ -56,20 +63,21 @@ void inicializarCuadrado() {
 
 void inicializarTriangulo() {
 	Vertice v1 = {
-		vec3(0.0f,0.3f,0.0f),
+		vec4(0.0f,0.3f,0.0f,1.0f),
 		vec4(0.8f,0.1f,0.0f,1.0f)
 	};
 	Vertice v2 = {
-		vec3(-0.3f,-0.3f,0.0f),
+		vec4(-0.3f,-0.3f,0.0f,1.0f),
 		vec4(0.8f,0.1f,0.0f,1.0f)
 	};
 	Vertice v3 = {
-		vec3(0.3f,-0.3f,0.0f),
+		vec4(0.3f,-0.3f,0.0f,1.0f),
 		vec4(0.8f,0.1f,0.0f,1.0f)
 	};
 	triangulo.push_back(v1);
 	triangulo.push_back(v2);
 	triangulo.push_back(v3);
+	transformacionesTriangulo = mat4(1.0f);
 }
 
 void dibujar() {
@@ -132,6 +140,7 @@ int main()
 	//Mapeo de atributos
 	posicionID = glGetAttribLocation(shader->getID(), "posicion");
 	colorID = glGetAttribLocation(shader->getID(), "color");
+	transformacionesID = glGetUniformLocation(shader->getID(), "transformaciones");
 
 	shader->desenlazar();
 	//Crear el vertex array del triangulo
